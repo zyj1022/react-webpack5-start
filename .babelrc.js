a008af30a39da4bf9c4d9085e1d7b74c08bfa319
zhiyanjie <zhiyanjie@jd.com>
2021-11-02 19:52:40 +0800

    mobx-value

--- .babelrc.js
+++ .babelrc.js
@@ -1,68 +1,35 @@
 const path = require('path');
 
 module.exports = {
-  presets: [ "@babel/preset-typescript", "@babel/preset-react" ],
-  env: {
-      production: {
-        presets: [
-          [
-            "@babel/preset-env",
-            {
-              "modules": false,
-              "useBuiltIns": "usage",
-              "corejs": 3
-            }
-          ]
-        ]
-    },
-    development: {
-      presets: [
-          [
-            "@babel/preset-env",
-            {
-              "modules": false
-            }
-          ]
-        ]
-    }
-  },
+  presets: ['@babel/preset-env', '@babel/preset-react', ['@babel/preset-typescript', { allowNamespaces: true }]],
   plugins: [
-    "@babel/plugin-transform-runtime",
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-syntax-dynamic-import',
     [
-			'module-resolver',
-			{
-				alias: {
-					'@': path.join(__dirname, './src')
-				},
-				extensions: [ '.js', '.jsx', '.ts', '.tsx', '.es', '.es6', '.mjs' ]
-			}
-		],
-    [
-      "@babel/plugin-proposal-decorators",
+      'module-resolver',
       {
-        "legacy": true
-      }
+        alias: {
+          '@': path.join(__dirname, './src'),
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.es', '.es6', '.mjs'],
+      },
     ],
     [
-      "@babel/plugin-proposal-class-properties",
+      'import',
       {
-        "loose": true
-      }
+        libraryName: 'antd',
+        style: true,
+      },
+      'antd',
     ],
-    "@babel/plugin-proposal-object-rest-spread",
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    '@babel/plugin-syntax-import-meta',
+    '@babel/plugin-proposal-json-strings',
+    '@babel/plugin-transform-object-assign',
+    '@babel/plugin-proposal-optional-chaining', // js 可选链 ?.
     ['@babel/plugin-proposal-private-methods', { loose: true }],
-    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
-    "@babel/plugin-proposal-nullish-coalescing-operator",
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-syntax-dynamic-import",
-    "react-hot-loader/babel",
-    [
-      "import",
-      {
-        "libraryName": "antd",
-        "libraryDirectory": "es",
-        "style": true
-      }
-    ]
-  ]
-}
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+  ],
+};
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { observer, inject } from 'mobx-react';
+import { toJS } from 'mobx';
 import styled from 'styled-components';
+import { observer } from 'mobx-react-lite';
 
 import BreadcrumdBar from '@/components/breadcrumd';
+import * as api from '../requests/common';
+
+import { mobxSetter, mobxRequest } from 'mobx-value';
 
 import { useStore } from '../stores';
 
@@ -10,22 +14,42 @@ const Containter = styled.div`
   text-align: center;
 `;
 
-const Home = () => {
-  const { common, home } = useStore();
-  const [userInfo, setUserInfo] = useState(home.userInfo);
-  useEffect(() => {
-    home.getUserInfo({ user: 'admin' }).then(() => {
-      setUserInfo(home.userInfo);
+export const getUserData = mobxRequest({
+  value: {},
+  autoCancelOnBecomeUnobserved: true,
+  request: async (params) => {
+    return await api.getUserInfo(params).then((res) => {
+      const result = res.data.data;
+      user.set(result);
+      return result;
     });
+  },
+});
+
+export const user = mobxSetter({
+  value: {},
+});
+
+const Home = observer(() => {
+  const { common, home } = useStore();
+  const [userInfo, setUserInfo] = useState({});
+  useEffect(() => {
+    // home.getUserInfo({ user: 'admin' }).then(() => {
+    //   setUserInfo(home.userInfo);
+    // });
+    getUserData.request();
   }, []);
+
+  console.log('getUserData--11', toJS(getUserData.value), toJS(user.value));
+
   return (
     <Containter>
       <BreadcrumdBar />
-      <h1> hello {userInfo.name}</h1>
-      <h2> hello {userInfo.pin}</h2>
+      <h1> hello {getUserData.value.name}</h1>
+      <h2> hello {getUserData.value.pin}</h2>
     </Containter>
   );
-};
+});
 
 // @inject('store')
 // @observer
